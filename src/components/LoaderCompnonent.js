e83ac9be520569e60e07e5c43aa73b9583d31ea9
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-08-17 14:52:55 +0530

    Initial Commit

---
+++ src/components/LoaderCompnonent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Loader() {
+  return(
+    <div className="container mx-auto">
+      <div className="align-middle pt-64 pb-64">
+        <h1 className="text-4xl textColor font-bold text-center">Attendance</h1>
+      </div>
+      <p className="text-center textColor font-bold text-xl pt-24">An initiative by ACM-VIT</p>
+    </div>
+  );
+}
+
+export default Loader;
