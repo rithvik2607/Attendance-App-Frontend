fc543d3366db448dd006bcc2555c6dec36c5bb81
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-08-20 15:24:56 +0530

    Website has been optimised as a PWA

--- src/components/ScanPageComponent.js
+++ src/components/ScanPageComponent.js
@@ -5,7 +5,7 @@ class ScanPage extends Component {
   render() {
     return(
       <div className="flex justify-center">
-        <QRCode size="200" renderAs="svg" fgColor="#201E50" value="http://facebook.github.io/react/" />
+        <QRCode size={200} renderAs="svg" fgColor="#201E50" value="http://facebook.github.io/react/" />
         <h1 className="text-center absolute inset-x-0 bottom-0 textColor font-bold text-xl pb-24">Your friends can scan this QR Code</h1>
       </div>
     );
