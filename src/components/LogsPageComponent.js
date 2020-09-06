0c1ef9cb6aa22bb1250b4a713bcc7c3cfe1153a0
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-09-06 13:29:30 +0530

    Added animations for the pages'

--- src/components/LogsPageComponent.js
+++ src/components/LogsPageComponent.js
@@ -1,44 +1,47 @@
 import React from 'react';
+import FadeIn from 'react-fade-in';
 
 function LogsPage() {
   return(
     <div className="mx-2">
-      <div className="bg-gray-800 shadow-xl rounded-md border-l-8 mx-4 border-gray-800">
-        <div style={{}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Evening Snacks</span>
-          <span>17:00 - 18:00</span>
+      <FadeIn>
+        <div className="bg-gray-800 shadow-xl rounded-md border-l-8 mx-4 border-gray-800">
+          <div style={{}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Evening Snacks</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-600 shadow-xl rounded-md border-l-8 mx-4 border-gray-600">
-        <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Breakfast</span>
-          <span>17:00 - 18:00</span>
+        <div className="bg-gray-600 shadow-xl rounded-md border-l-8 mx-4 border-gray-600">
+          <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Breakfast</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-600 shadow-xl rounded-md border-l-8 mx-4 border-gray-600">
-        <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Review I</span>
-          <span>17:00 - 18:00</span>
+        <div className="bg-gray-600 shadow-xl rounded-md border-l-8 mx-4 border-gray-600">
+          <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Review I</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
-        <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Review II</span>
-          <span>17:00 - 18:00</span>
+        <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
+          <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Review II</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
-        <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Review II</span>
-          <span>17:00 - 18:00</span>
+        <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
+          <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Review II</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
-      <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
-        <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
-          <span>Review II</span>
-          <span>17:00 - 18:00</span>
+        <div className="bg-gray-600 pl-1 shadow-xl rounded-md mx-4">
+          <div style={{borderColor:"#9D9Da2"}} className="bg-white p-4 my-4 flex text-lg justify-between items-center rounded-md">
+            <span>Review II</span>
+            <span>17:00 - 18:00</span>
+          </div>
         </div>
-      </div>
+      </FadeIn>
     </div>
   );
 }
--- src/components/ScanPageComponent.js
+++ src/components/ScanPageComponent.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import QRCode from 'qrcode.react';
+import FadeIn from 'react-fade-in';
 
 class ScanPage extends Component {
   render() {
     return(
       <div className="flex justify-center">
-        <QRCode size={200} renderAs="svg" fgColor="#201E50" value="http://facebook.github.io/react/" />
-        <h1 className="text-center absolute inset-x-0 bottom-0 textColor font-bold text-xl pb-24">Your friends can scan this QR Code</h1>
+        <FadeIn>
+          <div>
+            <QRCode size={200} renderAs="svg" fgColor="#201E50" value="http://facebook.github.io/react/" />
+          </div>
+          <div className="pt-24">
+            <h1 className="text-center absolute inset-x-0 bottom-0 textColor font-bold text-xl">Your friends can scan this QR Code</h1>
+          </div>
+        </FadeIn>
       </div>
     );
   }
